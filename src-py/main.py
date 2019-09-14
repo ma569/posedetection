2d50a92b8e294eda0d5d87cce06704d483edad38
Musabbir <musabbirmajeed@googlemail.com>
2019-09-14 12:05:34 +0500

    Add basic python files

---
+++ src-py/main.py
@@ -0,0 +1,34 @@
+# import sys
+# import cv2
+# import os
+# from sys import platform
+# import argparse
+
+# from openpose import pyopenpose
+
+# import openpose
+
+# # Import Openpose (Windows/Ubuntu/OSX)
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# print(dir_path)
+
+# From Python
+# It requires OpenCV installed for Python
+import sys
+import os
+
+
+print('\n'.join(sys.path))
+print('\n')
+print('\n'.join((os.environ['PYTHONPATH']).split(os.pathsep)))
+
+import openpose
+
+import cv2
+
+
+
+# import cv2
+# print(cv2)
+
+
